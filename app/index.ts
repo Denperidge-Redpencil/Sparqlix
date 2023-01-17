04883011ecbfa46f0bbc6dfcfacecdd9d03ed64b
Denperidge <denperidge@gmail.com>
2023-01-17 21:15:17 +0100

    Updated to matrix-bot-starter v1.1.0 syntax

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,6 @@
 // index.ts
 import { MatrixClient } from 'matrix-bot-sdk';
-import { newClient, multiMessageCommandSetup, onMessage, changeAvatar, changeDisplayname } from 'matrix-bot-starter';
+import { newClient, awaitMoreInput, onMessage, changeAvatar, changeDisplayname } from 'matrix-bot-starter';
 
 import { handleSparqlCodeblocks } from './sparql';
 
@@ -12,7 +12,7 @@ async function onEvents(client : MatrixClient) {
             if (mentioned) {
                 let command = mentioned.toLowerCase();
 
-                multiMessageCommandSetup(client, roomId, event, 
+                awaitMoreInput(client, roomId, event, 
                     (command.includes('picture') || command.includes('avatar')), 
                     true, 
                     {
@@ -22,7 +22,7 @@ async function onEvents(client : MatrixClient) {
                     }, 
                     'Setting new avatar! If your next message is an image, I will update my avatar to that.');
                 
-                multiMessageCommandSetup(client, roomId, event,
+                awaitMoreInput(client, roomId, event,
                     (command.includes('name') || command.includes('handle')), 
                     true, 
                     {
