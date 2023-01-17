da0fefd50a720357b51668009f4a991065d1124c
Denperidge <denperidge@gmail.com>
2023-01-17 20:05:32 +0100

    Queries now only return the requested properties

--- app/sparql.ts
+++ app/sparql.ts
@@ -2,7 +2,31 @@ import { MatrixClient } from "matrix-bot-sdk";
 import { logError } from "matrix-bot-starter";
 import StreamClient, { default as SparqlClient } from 'sparql-http-client';
 
+function streamDataToTdString(streamData: any, headers : Set<string>) {
+    if (typeof(streamData) == 'object') {
+        const entries : [string, any][] = Object.entries(streamData); 
 
+        
+        let tds = '';
+        for (let i = 0; i < entries.length; i++) {
+            const entry = entries[i];
+            const key = entry[0];
+            const value = entry[1];
+
+
+            // Don't add non-requested data
+            if (!headers.has(key) && key != 'value') { continue; }
+
+            tds += streamDataToTdString(value, headers);
+            
+        }
+        return tds;
+
+            
+    } else {
+        return `<td>${streamData}</td>`
+    }
+}
 
 export async function handleSparqlCodeblocks(client: MatrixClient, roomId : string, requestEventId : string, event : any, body : string, isEdit : Boolean) {
     const firstNewLine : number = body.indexOf('\n');
@@ -21,66 +45,46 @@ export async function handleSparqlCodeblocks(client: MatrixClient, roomId : stri
     
         const sparqlClient = new SparqlClient({endpointUrl});
         const stream = await sparqlClient.query.select(query);
-        
-            //console.log(stream)
+
         stream.on('error', (err) => {
             logError(err);
             //client.replyHtmlText
         });
-        /*
-        stream.on('data', row => {
-            console.log(row);
-            data += `<tr>${row}</tr>`;
-            //client.replyHtmlText(roomId, event, `test`)
-        });
-        stream.on('end' , () => {
-            stream.read()
-        });*/
+    
         stream.on('readable', function(this: any) {
             console.log(Object.prototype.toString.call(this))
 
-            let data : string = '<table>';
-            let streamData : any;
-            let headersSet : boolean = false;
+            let data : string = '';
+            let headers : Set<string> = new Set();
+            let streamData : Array<Array<any>>;
 
             while ((streamData = this.read()) !== null) {
-                if (!headersSet) {
-                    data += '<tr>'
-                    data += valueToRow(streamData, 0).replace(/td/gim, 'th');
-                    data += '</tr>'
-                    headersSet = true;
-                }
                 console.log(streamData);
-                function valueToRow(value: any, index=1) {
 
-                    if (typeof(value) == 'object') {
-                        console.log('value is object');
-                        const entries : [string, any][] = Object.entries(value); 
-                        console.log(entries)
-                        let tds = '';
-                        for (let i = 0; i < entries.length; i++) {
-                            tds += valueToRow(entries[i][index]);
-                        }
-                        return tds;
-
-                            
-                    } else {
-                        console.log('Not object')
-                        return `<td>${value}</td>`
-                    }
-                    
-                }
+                // Setup table headers
+                Object.entries(streamData).forEach((value: Array<any>) => {
+                    headers.add(value[0]);
+                });
+                
                 
                 data += '<tr>';
-                data += valueToRow(streamData)
+                data += streamDataToTdString(streamData, headers);
                 data += '</tr>'
+                
 
 
             }
-            if (!headersSet) {
+            if (data == '') {
                 // If there were no headers set, there was no data
                 return;
             }
+            
+            let headersHtml = '';
+            Array.from(headers).forEach((value, index) => {
+                headersHtml += `<th>${value}</th>`;
+            });
+            
+            data = `<table><tr>${headersHtml}</tr>${data}`;
             data += `</table>`;
             console.log('data: ' + data)
             client.replyHtmlText(roomId, event, data);
